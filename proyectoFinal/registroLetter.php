1af77a5f6b353d1e9ea4ff24d8bd7ad312643c3f
Francisco Javier Fernández López <115141653+Javixu04@users.noreply.github.com>
2024-02-16 14:41:05 +0100

    Form validation from PHP (16/02/24)

--- proyectoFinal/registroLetter.php
+++ proyectoFinal/registroLetter.php
@@ -21,6 +21,7 @@
                 <input type="password" placeholder="Contraseña" id="password" name="password" required><br><br>
                 <input type="password" placeholder="Confirmar contraseña" id="confirmPassword" required><br><br>
                 <a href="inicioSesion.php" class="inicio">Iniciar Sesión</a><br><br>
+                <div class="g-recaptcha" data-sitekey="TU_CLAVE_DE_SITIO"></div><br><br>
                 <input type="submit" value="Regístrate" id="Registro">
             </form> <img src="cartelLetter.png" alt="cartel" class="cartel">
 
@@ -30,28 +31,28 @@
                 if (false === $email) { //si el correo no es válido.
                     echo 'El correo electrónico no es válido';
                 } else {
-                    echo 'El correo electrónico validado es:' . $email;
+                    //Se guarda la información del usuario y BBDD.
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") { //Verificación de si el formulario ha sido enviado
+                        // Obtenemos el nombre de usuario y contraseña desde el formulario
+                        $nombre = $_POST["usuario"];
+                        $user_password = $_POST["password"];
+                        $servidor = "localhost";
+                        $usuario = "root";
+                        $pass = "";
+
+                        // Conexión a la base de datos
+                        $conectar = mysqli_connect($servidor, $usuario, $pass) or die("Error de conexión");
+                        mysqli_select_db($conectar, "proyecto");
+                        // Consulta para insertar el usuario en la base de datos
+                        $sql = "INSERT INTO usuarios (nombre, contraseña) VALUES ('$nombre', '$user_password');";
+                        mysqli_query($conectar, $sql);
+                        // Redirección a inicioSesion.php después del registro
+                        header("Location: inicioSesion.php");
+                        exit();
+                    }
                 }
             }
-            //Se guarda la información del usuario y BBDD.
-            if ($_SERVER["REQUEST_METHOD"] == "POST") { //Verificación de si el formulario ha sido enviado
-                // Obtenemos el nombre de usuario y contraseña desde el formulario
-                $nombre = $_POST["usuario"];
-                $user_password = $_POST["password"];
-                $servidor = "localhost";
-                $usuario = "root";
-                $pass = "";
 
-                // Conexión a la base de datos
-                $conectar = mysqli_connect($servidor, $usuario, $pass) or die("Error de conexión");
-                mysqli_select_db($conectar, "proyecto");
-                // Consulta para insertar el usuario en la base de datos
-                $sql = "INSERT INTO usuarios (nombre, contraseña) VALUES ('$nombre', '$user_password');";
-                mysqli_query($conectar, $sql);
-                // Redirección a inicioSesion.php después del registro
-                header("Location: inicioSesion.php");
-                exit();
-            }
             ?>
 
         </div>
